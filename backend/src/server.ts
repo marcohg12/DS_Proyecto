8178a60a8347aa3ce771c5e14923b1b7aeb05749
Marco <marcohg0312@gmail.com>
2023-10-25 01:01:13 -0600

    mas tests

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -31,7 +31,7 @@ app.use(
 
 app.use(
   session({
-    cookie: { maxAge: 86400000, secure: true },
+    cookie: { maxAge: 86400000 },
     store: new MemoryStore({
       checkPeriod: 86400000, // prune expired entries every 24h
     }),
